96653060304a308af448a81dc6595395f14f950a
Ikmal Syifai <ikmal.s.16@gmail.com>
2020-07-29 18:31:36 +0700

    Add JSON schema validator action

---
+++ .github/actions/json-schema-validator/validate.py
@@ -0,0 +1,8 @@
+from jsonschema import Draft7Validator
+import os
+
+json_schema = os.getenv('INPUT_JSON_SCHEMA')
+json_files = os.getenv('INPUT_JSON_FILES')
+event = os.getenv('GITHUB_EVENT_PATH"')
+
+print(event)
