b3f85c1c1f475e2e3803f45b27d545a08db5403f
Ikmal Syifai <ikmal.s.16@gmail.com>
2020-07-30 12:51:53 +0700

    Use header

--- .github/actions/json-schema-validator/validate.py
+++ .github/actions/json-schema-validator/validate.py
@@ -18,10 +18,10 @@ def query(headers, url, data=None):
     if data is not None:
         response = requests.post(url, json=data, headers=headers)
     else:
-        # response = requests.get(url, headers=headers)
-        print('headers')
-        print(headers)
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
+        # print('headers')
+        # print(headers)
+        # response = requests.get(url)
 
     if response.status_code == 200:
         return response.json()
