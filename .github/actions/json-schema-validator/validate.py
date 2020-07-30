d0901bfce1a1a587ede2e28adb134bb1117e9f5f
Ikmal Syifai <ikmal.s.16@gmail.com>
2020-07-30 14:52:11 +0700

    Send comment when there's validation errors

--- .github/actions/json-schema-validator/validate.py
+++ .github/actions/json-schema-validator/validate.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 
+import jq
 import requests
 from jsonschema import Draft7Validator
 
@@ -9,12 +10,14 @@ json_schema = os.getenv('INPUT_JSON_SCHEMA')
 json_path_pattern = os.getenv('INPUT_JSON_PATH_PATTERN')
 send_comment = os.getenv('INPUT_SEND_COMMENT')
 clear_comments = os.getenv('INPUT_CLEAR_COMMENTS')
-token = os.getenv('INPUT_TOKEN')
 
 event_path = os.getenv('GITHUB_EVENT_PATH')
+repo = os.getenv('GITHUB_REPOSITORY')
 
 
-def query(headers, url, data=None):
+def query(url, data=None):
+    headers = {'Authorization': 'Bearer {}'.format(os.getenv('INPUT_TOKEN'))}
+
     if data is not None:
         response = requests.post(url, json=data, headers=headers)
     else:
@@ -36,6 +39,7 @@ def validate_file(json_schema, json_path_pattern, file_path):
         validator = Draft7Validator(schema)
         for error in sorted(validator.iter_errors(instance), key=str):
             validation_error = {}
+            validation_error['path'] = file_path
             validation_error['message'] = error.message
             validation_error['validator'] = error.validator
             validation_error['validator_value'] = error.validator_value
@@ -44,7 +48,8 @@ def validate_file(json_schema, json_path_pattern, file_path):
 
         return validation_errors
     else:
-        print('{} doesn\'t match pattern {}'.format(file_path, json_path_pattern))
+        print('{} doesn\'t match pattern {}'.format(
+            file_path, json_path_pattern))
         return []
 
 
@@ -53,7 +58,23 @@ def clear_comments():
 
 
 def send_comment(errors):
-    return True
+    formatted_errors = []
+    for error in errors:
+        path = error['path']
+        message = error['message']
+        validator = error['validator']
+        validator_value = error['validator_value']
+
+        formatted = MESSAGE.format(
+            path=path, message=message, validator=validator, validator_value=validator_value)
+        formatted_errors.append(formatted)
+
+    joined_errors = '\r\n\r\n'.join(formatted_errors)
+    
+    comment_url = ISSUE_COMMENTS.format(repo=repo, issue_number=pull_number)
+    body = {'body': joined_errors}
+    res = query(comment_url, body)
+    print(res)
 
 
 def json_from_file(file_path):
@@ -61,14 +82,24 @@ def json_from_file(file_path):
         return json.load(f)
 
 
+BASE = 'https://api.github.com'
+PR_FILES = BASE + 'repos/{repo}/pulls/{pull_number}/files'
+ISSUE_COMMENTS = BASE + 'repos/{repo}/issues/{issue_number}/comments'
+MESSAGE = '''**JSON Schema validation failed for `{path}`.**
+            **Message** : `{message}`
+            **Validator** : `{validator}`
+            **Validator Rule** : `{validator_value}`'''
+
 event = json_from_file(event_path)
-changed_files_url = event['pull_request']['_links']['self']['href'] + '/files'
-headers = {"Authorization": "Bearer {}".format(token)}
+pull_number = jq.compile('.pull_request.number').input(event).first()
+
 
 errors = []
-changed_files = query(headers, changed_files_url)
-for changed_file in changed_files:
-    filename = changed_file['filename']
+pr_files_url = PR_FILES.format(repo=repo, pull_number=pull_number)
+pr_files = query(pr_files_url)
+
+for pr_file in pr_files:
+    filename = pr_file['filename']
     validation_errors = validate_file(json_schema, json_path_pattern, filename)
 
     if len(validation_errors):
@@ -80,3 +111,5 @@ if len(errors):
 
     for error in errors:
         print(error)
+
+    # raise Exception('Fail validation')
