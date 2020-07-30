9df7f9d51e0acb2c2f097a5d5d581bf566a9b2ca
Ikmal Syifai <ikmal.s.16@gmail.com>
2020-07-30 11:49:10 +0700

    Add validation

--- .github/actions/json-schema-validator/validate.py
+++ .github/actions/json-schema-validator/validate.py
@@ -4,21 +4,36 @@ import os
 import json
 
 json_schema = os.getenv('INPUT_JSON_SCHEMA')
-json_files = os.getenv('INPUT_JSON_FILES')
+json_path_pattern = os.getenv('INPUT_JSON_PATH_PATTERN')
+send_comment = os.getenv('INPUT_SEND_COMMENT')
+clear_comments = os.getenv('INPUT_CLEAR_COMMENTS')
+
 event_path = os.getenv('GITHUB_EVENT_PATH')
 token = os.getenv('GITHUB_TOKEN')
 
 
 def query(headers, url, data=None):
     if data is not None:
-        response = requests.post(url, data)
+        response = requests.post(url, json=data, headers=headers)
     else:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         return response.json()
     else:
         raise Exception(response.status_code + ':' + query)
+
+
+def validate_file(json_schema, json_path_pattern, file_path):
+    return []
+
+
+def clear_comments():
+    return True
+
+
+def send_comment(errors):
+    return True
 
 
 with open(event_path) as f:
@@ -27,5 +42,18 @@ with open(event_path) as f:
 changed_files_url = event['pull_request']['_links']['self']['href'] + '/files'
 headers = {"Authorization": "token {}".format(token)}
 
+errors = []
 changed_files = query(headers, changed_files_url)
-print(changed_files)
+for changed_file in changed_files:
+    filename = changed_file['filename']
+    validation_errors = validate_file(json_schema, json_path_pattern, filename)
+
+    if len(validation_errors):
+        errors.append(validation_errors)
+
+if len(errors):
+    if send_comment:
+        create_comment(errors)
+
+    for error in errors:
+        print(error)
