f6cfb14c146adb851b0e7c018cd94b660c2d3cf9
Ikmal Syifai <ikmal.s.16@gmail.com>
2020-07-30 15:54:49 +0700

    Trigger comment

--- .github/actions/json-schema-validator/validate.py
+++ .github/actions/json-schema-validator/validate.py
@@ -15,13 +15,15 @@ event_path = os.getenv('GITHUB_EVENT_PATH')
 repo = os.getenv('GITHUB_REPOSITORY')
 
 
-def query(url, data=None):
+def request(verb, url, data=None):
     headers = {'Authorization': 'Bearer {}'.format(os.getenv('INPUT_TOKEN'))}
-
-    if data is not None:
-        response = requests.post(url, json=data, headers=headers)
-    else:
-        response = requests.get(url, headers=headers)
+    verb_map = {
+        'get': requests.get,
+        'post': requests.post,
+        'delete': requests.delete
+    }
+    
+    response = verb_map.get(verb)(url, json=data, headers=headers)
 
     if response.status_code >= 200 and response.status_code < 300:
         return response.json()
@@ -52,9 +54,23 @@ def validate_file(json_schema, json_path_pattern, file_path):
             file_path, json_path_pattern))
         return []
 
+def delete_comment(comment_id):
+    comment_url = ISSUE_COMMENTS.format(repo=repo, issue_number=pull_number)
+    request('delete', '{}/{}'.format(comment_url, comment_id))
 
 def clear_comments():
-    return True
+    bot = 'github-actions[bot]'
+    comment_url = ISSUE_COMMENTS.format(repo=repo, issue_number=pull_number)
+
+    comments = request('get', comment_url)
+    jq_user = jq.compile('.user.login')
+    jq_comment = jq.compile('.id')
+
+    for comment in comments:
+        user = jq_user.input(comment).first()
+        if user == bot:
+            comment_id = jq_comment.input(comment).first()
+            delete_comment(comment_id)
 
 
 def send_comment(errors):
@@ -74,8 +90,7 @@ def send_comment(errors):
 
     comment_url = ISSUE_COMMENTS.format(repo=repo, issue_number=pull_number)
     body = {'body': joined_errors}
-    res = query(comment_url, body)
-    print(res)
+    request('post', comment_url, body)
 
 
 def json_from_file(file_path):
@@ -97,7 +112,7 @@ pull_number = jq.compile('.pull_request.number').input(event).first()
 
 errors = []
 pr_files_url = PR_FILES.format(repo=repo, pull_number=pull_number)
-pr_files = query(pr_files_url)
+pr_files = request('get', pr_files_url)
 
 for pr_file in pr_files:
     filename = pr_file['filename']
@@ -105,6 +120,9 @@ for pr_file in pr_files:
 
     if len(validation_errors):
         errors.append(validation_errors)
+
+if clear_comments:
+    clear_comments()
 
 if len(errors):
     if send_comment:
